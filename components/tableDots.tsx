eb99fd0551067fd161c83b1a70a38c64a695a083
Nicolas Ludueña <nicolas.ludena@woowup.com>
2024-06-25 17:22:55 -0300

    Refactor TableDots component for improved readability and maintainability

--- components/tableDots.tsx
+++ components/tableDots.tsx
@@ -1,117 +1,118 @@
 import React, { useEffect, useState, useCallback } from "react";
 import {
-    Table,
-    TableHeader,
-    TableCell,
-    TableColumn,
-    TableBody,
-    TableRow
+  Table,
+  TableHeader,
+  TableCell,
+  TableColumn,
+  TableBody,
+  TableRow,
 } from "@nextui-org/table";
 import { Tooltip } from "@nextui-org/tooltip";
 import { DeleteIcon } from "./icons";
 
-export default function TableDots(props: { dots: any, setDot: (params: any) => void }) {
-    const [dotsTable, setDotsTable] = useState([]);
+export default function TableDots(props: {
+  dots: any;
+  setDot: (params: any) => void;
+}) {
+  const formateDot = (initialize: boolean = false) => {
+    let variable = props.dots.match(/{([^}]*)}/);
+    if (!variable) return;
 
-    const renderCell = useCallback((dot: any, columnKey: any) => {
-        const cellValue = dot[columnKey];
+    console.log(props.dots);
+    variable = variable[1];
+    variable = variable.split(";");
+    variable.shift();
+    variable.pop();
+    const array = variable.map((e: any, index: any) => {
+      let peso = e.match(/\[label=(\d+)\]/);
+      let origen = e.match(/^[^-]*/)[0];
+      let destino = "";
+      if (e.includes("->")) {
+        destino = e.match(/->([^[]*)/)[1];
+      } else {
+        destino = e.match(/--([^[]*)/)[1];
+      }
+      return {
+        id: index,
+        origen: origen.trim(),
+        destino: destino.trim(),
+        peso: peso ? peso[1] : "",
+        dot: e,
+      };
+    });
+    if (!initialize) setDotsTable(array);
+    return array;
+  };
+  const [dotsTable, setDotsTable] = useState(formateDot(true));
 
-        switch (columnKey) {
-            case "acciones":
-                return (
-                    <div className="w-max">
-                        <Tooltip content="Eliminar Nodo" color="danger">
-                            <span className="cursor-pointer text-danger">
-                                <DeleteIcon onClick={() => eliminarDot(dot.dot)} />
-                            </span>
-                        </Tooltip>
-                    </div>
-                );
-            default:
-                return cellValue;
-        }
-    }, []);
+  const renderCell = useCallback(
+    (dot: any, columnKey: any) => {
+      const cellValue = dot[columnKey];
+      switch (columnKey) {
+        case "acciones":
+          return (
+            <div className="w-max">
+              <Tooltip content="Eliminar Nodo" color="danger">
+                <span className="cursor-pointer text-danger">
+                  <DeleteIcon onClick={() => eliminarDot(dot.dot)} />
+                </span>
+              </Tooltip>
+            </div>
+          );
+        default:
+          return cellValue;
+      }
+    },
+    [dotsTable]
+  );
 
-    const formateDot = () => {
-        let variable = props.dots.match(/{([^}]*)}/);
-        if (!variable) return;
+  const eliminarDot = (dot: string) => {
+    console.log("Dot to delete:", dot);
+    dot += ";";
+    props.setDot(dot);
+  };
 
-        console.log(props.dots);
-        variable = variable[1];
-        variable = variable.split(';');
-        variable.shift();
-        variable.pop();
-        const array = variable.map((e: any, index: any) => {
-            let peso = e.match(/\[label=(\d+)\]/);
-            let origen = e.match(/^[^-]*/)[0];
-            let destino = "";
-            if (e.includes("->")) {
-                destino = e.match(/->([^[]*)/)[1];
-            } else {
-                destino = e.match(/--([^[]*)/)[1];
-            }
-            return ({
-                id: index,
-                origen: origen.trim(),
-                destino: destino.trim(),
-                peso: peso ? peso[1] : "",
-                dot: e
-            });
-        });
-        setDotsTable(array);
-    };
+  const columns = [
+    {
+      key: "origen",
+      label: "ORIGEN",
+    },
+    {
+      key: "destino",
+      label: "DESTINO",
+    },
+    {
+      key: "peso",
+      label: "PESO",
+    },
+    {
+      key: "acciones",
+      label: "ACCIONES",
+    },
+  ];
 
-    const eliminarDot = (dot: any) => {
-        console.log("Dot to delete:", dot);
-        props.setDot(dot);
-    };
+  useEffect(() => {
+    formateDot();
+  }, [props.dots]);
 
-
-    const columns = [
-        {
-            key: "origen",
-            label: "ORIGEN",
-        },
-        {
-            key: "destino",
-            label: "DESTINO",
-        },
-        {
-            key: "peso",
-            label: "PESO",
-        },
-        {
-            key: "acciones",
-            label: "ACCIONES",
-        },
-    ];
-
-    useEffect(() => {
-        formateDot();
-    }, [props.dots]);
-
-    return (
-        <Table
-            aria-label="Example table with custom cells, pagination and sorting"
-            isHeaderSticky
-            bottomContentPlacement="outside"
-        >
-            <TableHeader columns={columns}>
-                {(column) => (
-                    <TableColumn
-                        key={column.key}
-                    >
-                        {column.label}
-                    </TableColumn>
-                )}
-            </TableHeader>
-            <TableBody emptyContent={"No users found"} items={dotsTable}>
-                {(item: any) => (
-                    <TableRow key={item.id}>
-                        {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
-                    </TableRow>
-                )}
-            </TableBody>
-        </Table>
-    );
+  return (
+    <Table
+      aria-label="Example table with custom cells, pagination and sorting"
+      isHeaderSticky
+      bottomContentPlacement="outside"
+    >
+      <TableHeader columns={columns}>
+        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+      </TableHeader>
+      <TableBody emptyContent={"No graph found"} items={dotsTable}>
+        {(item: any) => (
+          <TableRow key={item.id}>
+            {(columnKey) => (
+              <TableCell>{renderCell(item, columnKey)}</TableCell>
+            )}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
 }
